1102772084f8710a1fcbb57b0d4870cb9152dab5
luke3 <luke3@ualberta.ca>
2023-06-22 09:32:43 -0600

    Separate initialization

--- wrapper.cpp
+++ wrapper.cpp
@@ -18,8 +18,74 @@
 
 using namespace std;
 
-int main() {
+int n, fd1[2], fd2[2];
 
+void initialize() {
+    pid_t pid;
+    
+    if (pipe(fd1) < 0 || pipe(fd2) < 0)
+        cout << strerror(errno) << endl;
+
+    if ((pid = fork()) < 0) {
+        cout << strerror(errno) << endl;
+    } else if (pid > 0) {  // parent
+        close(fd1[0]);
+        close(fd2[1]);
+    } else {  // child
+        close(fd1[1]);
+        close(fd2[0]);
+        if (fd1[0] != STDIN_FILENO) {
+            if (dup2(fd1[0], STDIN_FILENO) != STDIN_FILENO)
+                cout << strerror(errno) << endl;
+            close(fd1[0]);
+        }
+
+        if (fd2[1] != STDIN_FILENO) {
+            if (dup2(fd2[1], STDOUT_FILENO) != STDOUT_FILENO)
+                cout << strerror(errno) << endl;
+            close(fd2[1]);
+        }
+        
+        char *benzenePath = "/home/luke/benzene-vanilla-cmake/build/src/mohex/mohex";
+        if (execvp(benzenePath, NULL) < 0) {
+            cout << strerror(errno) << endl;
+        }
+
+        exit(0);
+    }
+}
+
+void run_parent() {
+    char line[MAXLINE];
+
+    while (fgets(line, MAXLINE, stdin) != NULL) {
+        n = strlen(line);
+        if (write(fd1[1], line, n) != n)
+            cout << strerror(errno) << endl;
+        if ((n = read(fd2[0], line, MAXLINE)) < 0)
+            cout << strerror(errno) << endl;
+        if (n == 0) {
+            cout << strerror(errno) << endl;
+            break;
+        }
+
+        line[n] = 0;  // null terminate
+        if (fputs(line, stdout) == EOF)
+            cout << strerror(errno) << endl;
+    }
+
+    if (ferror(stdin))
+        cout << strerror(errno) << endl;
+    exit(0);
+
+}
+
+
+int main() {
+    initialize();
+    run_parent();
+
+    /*
     int     n, fd1[2], fd2[2];
     pid_t   pid;
     char    line[MAXLINE];
@@ -72,15 +138,7 @@ int main() {
             cout << strerror(errno) << endl;
         }
 
-        /*
-        char *interpreter = "python3";
-        char *pythonPath = "./test.py";
-        char *pythonArgs[] = {interpreter, pythonPath, NULL};
-        if (execvp(interpreter, pythonArgs) < 0) {
-            cout << strerror(errno) << endl;
-        }
-        */
-
         exit(0);
     }
+    */
 }
